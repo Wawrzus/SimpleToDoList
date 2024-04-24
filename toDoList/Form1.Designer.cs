2304a24a7fa6f553b332f0fce364dfc77f85b43c
Szymon Wawrzyczek <100782104+Wawrzus@users.noreply.github.com>
2024-04-24 17:04:38 +0200

    First commit
    First and only commit in this repo

---
+++ toDoList/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace wpf_app
+{
+    partial class mainWindow
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainWindow));
+            this.checkboxLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.checkedListBox = new System.Windows.Forms.CheckedListBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.addButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.labelEmptyInfo = new System.Windows.Forms.Label();
+            this.checkboxLayout.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkboxLayout
+            // 
+            this.checkboxLayout.ColumnCount = 1;
+            this.checkboxLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.checkboxLayout.Controls.Add(this.checkedListBox, 0, 0);
+            this.checkboxLayout.Location = new System.Drawing.Point(12, 12);
+            this.checkboxLayout.Name = "checkboxLayout";
+            this.checkboxLayout.RowCount = 1;
+            this.checkboxLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.checkboxLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 298F));
+            this.checkboxLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 298F));
+            this.checkboxLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 298F));
+            this.checkboxLayout.Size = new System.Drawing.Size(461, 298);
+            this.checkboxLayout.TabIndex = 0;
+            // 
+            // checkedListBox
+            // 
+            this.checkedListBox.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.checkedListBox.FormattingEnabled = true;
+            this.checkedListBox.Location = new System.Drawing.Point(3, 3);
+            this.checkedListBox.Name = "checkedListBox";
+            this.checkedListBox.Size = new System.Drawing.Size(454, 290);
+            this.checkedListBox.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.textBox, 0, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(479, 15);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 37F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 37F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 37F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(453, 37);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // textBox
+            // 
+            this.textBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox.Font = new System.Drawing.Font("Roboto", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox.Location = new System.Drawing.Point(3, 3);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(447, 30);
+            this.textBox.TabIndex = 0;
+            this.textBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 45F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 45F));
+            this.tableLayoutPanel1.Controls.Add(this.addButton, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.removeButton, 2, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(479, 70);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(453, 151);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // addButton
+            // 
+            this.addButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.addButton.Font = new System.Drawing.Font("Roboto Medium", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.addButton.Location = new System.Drawing.Point(0, 50);
+            this.addButton.Margin = new System.Windows.Forms.Padding(0, 50, 0, 50);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(203, 51);
+            this.addButton.TabIndex = 0;
+            this.addButton.Text = "Add to list";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // removeButton
+            // 
+            this.removeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.removeButton.Font = new System.Drawing.Font("Roboto Medium", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.removeButton.Location = new System.Drawing.Point(248, 50);
+            this.removeButton.Margin = new System.Windows.Forms.Padding(0, 50, 0, 50);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(205, 51);
+            this.removeButton.TabIndex = 1;
+            this.removeButton.Text = "Remove";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel3.Controls.Add(this.labelEmptyInfo, 0, 0);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(479, 227);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(453, 83);
+            this.tableLayoutPanel3.TabIndex = 3;
+            // 
+            // labelEmptyInfo
+            // 
+            this.labelEmptyInfo.AutoSize = true;
+            this.labelEmptyInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelEmptyInfo.Font = new System.Drawing.Font("Roboto Medium", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelEmptyInfo.Location = new System.Drawing.Point(3, 0);
+            this.labelEmptyInfo.Name = "labelEmptyInfo";
+            this.labelEmptyInfo.Size = new System.Drawing.Size(447, 83);
+            this.labelEmptyInfo.TabIndex = 0;
+            this.labelEmptyInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // mainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(944, 321);
+            this.Controls.Add(this.tableLayoutPanel3);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.checkboxLayout);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "mainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "To do list";
+            this.checkboxLayout.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel checkboxLayout;
+        private System.Windows.Forms.CheckedListBox checkedListBox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Label labelEmptyInfo;
+    }
+}
+
---
+++ toDoList/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace wpf_app
+{
+    public partial class mainWindow : Form
+    {
+        public mainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            string dataText = textBox.Text;
+            if(dataText == null || dataText == "")
+            {
+                labelEmptyInfo.Text = "ZADANIE NIE MOŻE BYĆ PUSTE";
+            }
+            else
+            {
+                checkedListBox.Items.Add(dataText);
+                textBox.Text = null;
+            }
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            ArrayList arrayList = new ArrayList();
+            foreach (var item in checkedListBox.CheckedItems)
+            {
+                arrayList.Add(item);
+            }
+
+            foreach (var deleteItem in arrayList)
+            {
+                checkedListBox.Items.Remove(deleteItem);
+            }
+        }
+    }
+}
